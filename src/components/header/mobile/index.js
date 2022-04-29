dd220de7c8bfebf216e29c0a5a638e018efc962b
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-29 09:07:37 +0530

    index update

---
+++ src/components/header/mobile/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Mobile() {
+  return (
+    <div>This is Mobile</div>
+  )
+}
+
+export default Mobile
---
+++ src/components/header/web/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Web() {
+  return (
+    <div>This is Web</div>
+  )
+}
+
+export default Web
