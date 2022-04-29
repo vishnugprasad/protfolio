cb923ce8231f573bb8e4c4df97c16e32556b7012
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-29 09:07:24 +0530

    index update

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./header.css";
+import Web from "./web/index";
+import Mobile from "./mobile";
 
 function Header() {
   return (
     <div className="header">
       <div className="logo"> VISHNU G PRASAD</div>
       <div className="menu">
-        <div className="web-menu"> web</div>
-        <div className="mobile-menu">Mobile</div>
+        <div className="web-menu"> <Web/></div>
+        <div className="mobile-menu"><Mobile/></div>
       </div>
     </div>
   );
