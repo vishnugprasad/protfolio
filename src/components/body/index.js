2cf17dd6666aa42a220710475e9352decbbcef93
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-29 07:32:32 +0530

    layout

---
+++ src/components/body/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <div>This is Body</div>
+  )
+}
+
+export default Body
---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo"> VISHNU G PRASAD</div>
+      <div className="menu">
+        <div className="web-menu"> web</div>
+        <div className="mobile-menu">Mobile</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
