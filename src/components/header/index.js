807215082aa7b7cc8c6cf4708e704ded3c9b3e15
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-30 08:19:57 +0530

    mobile view update

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import Web from "./web/index";
-import Mobile from "./mobile";
+import Mobile from "./mobile/index";
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="header">
       <div className="logo"> VISHNU G PRASAD</div>
       <div className="menu">
-        <div className="web-menu"> <Web/></div>
-        <div className="mobile-menu"><Mobile/></div>
+        <div className="web-menu">
+          <Web />
+        </div>
+        <div className="mobile-menu">
+          <div>
+          <button onClick = {()=> {setIsOpen(!isOpen)}}className = "menu-icon"></button>
+          </div>
+          <div>
+            {isOpen && <Mobile isOpen = {isOpen} setIsOpen = {setIsOpen} />}  
+            {/* passing props to the mobile */}
+          </div>
+          
+        </div>
       </div>
     </div>
   );
--- src/components/header/web/index.js
+++ src/components/header/web/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./web.css";
 
 function Web() {
   return (
-    <div>This is Web</div>
-  )
+    <div className="web">
+      <div className="web-option">
+        <a href="#project">
+        <i class="fi fi-sr-edit-alt option-icon"></i> PROJECTS
+        </a>
+      </div>
+
+      <div className="web-option">
+        <a href="#skills">
+        <i class="fi fi-rr-laptop option-icon "></i> SKILLS
+        </a>
+       
+      </div>
+
+      <div className="web-option">
+        <a href="#work">
+            WORK
+        </a>
+      </div>
+
+      <div className="web-option">
+        <a href="#connect">
+            CONNECT
+        </a>
+      </div>
+
+    </div>
+  );
 }
 
-export default Web
+export default Web;
