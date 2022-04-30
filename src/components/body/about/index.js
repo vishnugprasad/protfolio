79ab75fe20c7dbe1474ce54ce83c483db85c3dcd
vishnugprasad <vishnuanugraha001@gmail.com>
2022-04-30 16:34:03 +0530

    about updated

--- src/components/body/about/index.js
+++ src/components/body/about/index.js
@@ -6,10 +6,12 @@ function About() {
     <div className='about'>
         <div className='about-top'>
             <div className='about-info'>
-
+                My name is <span className='info-name'>Vishnu G Prasad</span>. I'm a Full Stack Developer based in India.
+                I am very passionate and dedicated to my work. And I own the necessary
+                skills to build great and premimum websites.
             </div>
             <div className='about-photo'>
-
+                <img src = {require("../../../assets/coding.png")} className = "about-picture"/>
             </div>
 
         </div>
