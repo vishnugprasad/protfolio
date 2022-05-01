520b9b67bced1289e86aef0d35e1c168a79de0e9
vishnugprasad <vishnuanugraha001@gmail.com>
2022-05-01 12:39:00 +0530

    project data imported

--- src/components/body/projects/project-card.js
+++ src/components/body/projects/project-card.js
@@ -15,9 +15,27 @@ function ProjectCard({project}) {
                         </div>
                     }</a>)}
 
+                    {project.github&&(<a className='project-link' href={project.github}>
+                    {
+                        <div className='link-button'>
+                            <i class="devicon-github-original colored"></i>
+                            Github
+                        </div>
+                    }</a>)}
+
+            </div>
+            <p>{project.about}</p>
+            <div className='project-tags'>
+
+                {project.tags.map((tag)=>{
+                    return (
+                        <label className='tag'> {tag}</label>
+                    )
+                })}
+
             </div>
             </div> 
-        <img src = {project.img} className = "prohject-photo"/>
+        <img src = {project.image} className = "project-photo"/>
         
     </div>
   )
--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,11 +1,12 @@
 export const ProjectData = [
     {
         id: 1,
-        title: " office seatbooking",
+        title: " OFFICE SEATBOOKING",
         about:"The project is a generalized platform focused on creating and managing real time seatbooking in the office , where an employee can choose to book his/her seat in advance by checking the availability. The aim was to make the hybrid work model more friendly to the employees . The front end of the application is developed with React and backend with node.js. This is deployed in Netlify",
         tags: ["Reactjs, Nodejs, JavaScript"],
         demo: "https://elastic-bardeen-e94347.netlify.app/ ",
         github: "https://github.com/vishnugprasad/Hours-subsidary-",
-        image: ""
+        image: require("../../assets/hours.png")
+       
     }
 ]
