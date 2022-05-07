0612570d2ca63a36cb9b47a8a98cbb885baf34a4
vishnugprasad <vishnuanugraha001@gmail.com>
2022-05-07 19:32:37 +0530

    link update

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -4,7 +4,7 @@ export const ProjectData = [
         title: " OFFICE SEATBOOKING",
         about:"The project is a generalized platform focused on creating and managing real time seatbooking in the office , where an employee can choose to book his/her seat in advance by checking the availability. The aim was to make the hybrid work model more friendly to the employees . The front end of the application is developed with React and backend with node.js. This is deployed in Netlify",
         tags: ["Reactjs", "Nodejs", "JavaScript"],
-        demo: "https://vishnugprasad.netlify.app/",
+        demo: "https://seatbookinghours.netlify.app/ ",
         github: "https://github.com/vishnugprasad/Hours-subsidary-",
         image: require("../../assets/hours.png")
        
